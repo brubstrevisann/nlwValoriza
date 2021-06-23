58b9f534a7f17a7878059cf371e65173d7185864
Brubs <bruno.amatoz@hotmail.com>
2021-06-22 23:52:54 -0300

    aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express, { response } from "express";
-
+import "reflect-metadata";
+import { router } from "./routes";
 // @types/express para poder acessar as tipagens
 const app = express();
+
+app.use(express.json())
+
+app.use(router);
+
+import "./database"
 
 app.get("/test", (request, response) => {
   return response.send("oie, sou um get")
