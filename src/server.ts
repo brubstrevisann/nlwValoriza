d9286bc7055cfbdc2ffc99675a52dd1ffaad9ec4
Brubs <bruno.amatoz@hotmail.com>
2021-06-23 23:36:59 -0300

    aula 03

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,42 @@
-import express, { response } from "express";
+import express, { Request, Response, NextFunction } from "express";
 import "reflect-metadata";
+import "express-async-errors";
+import "./database"
 import { router } from "./routes";
+
 // @types/express para poder acessar as tipagens
 const app = express();
 
+// habilia o uso de json nas requests
 app.use(express.json())
 
+// habilita as rotas
 app.use(router);
 
-import "./database"
+//midleware de error
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message
+      });
+    }
 
-app.get("/test", (request, response) => {
-  return response.send("oie, sou um get")
-})
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error"
+    })
 
-app.post("/testPost", (request, response) => {
-  return response.send("oie, sou um post")
-})
+  });
+
+
+// app.get("/test", (request, response) => {
+//   return response.send("oie, sou um get")
+// })
+
+// app.post("/testPost", (request, response) => {
+//   return response.send("oie, sou um post")
+// })
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"))
